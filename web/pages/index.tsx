2b0b3f5821bb428911728dd14f10d308732bcdc0
iamjianyoudai <daijianyou@gmail.com>
2024-05-14 10:23:25 +0200

    Initial commit

---
+++ web/pages/index.tsx
@@ -0,0 +1,10 @@
+export function Home() {
+  return (
+    <main className="flex items-center justify-center min-h-screen">
+      <h1 className="text-10xl font-bold text-center">
+        Welcome to WATERMOON VISION
+      </h1>
+    </main>
+  );
+}
+export default Home;
